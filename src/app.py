b4a53f90ad1b64e5faba61ec7f5d7cfe176008e2
anku-13 <72180865+anku-13@users.noreply.github.com>
2021-04-21 19:57:13 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,9 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    print("Fdfs")
-    return "Hello World"
+def add(first_term, second_term):
+    return first_term + second_term
 
 
 if __name__ == "__main__":
