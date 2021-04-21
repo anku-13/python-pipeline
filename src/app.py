d53e2cad4d0a0089cdb9ff8198a6fe1e864c3273
anku-13 <72180865+anku-13@users.noreply.github.com>
2021-04-21 20:16:52 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def add(first_term, second_term):
-    return first_term + second_term
+def index():
+    return "Hello World"
 
 
 if __name__ == "__main__":
